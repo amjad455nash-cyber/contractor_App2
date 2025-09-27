3515f277f64aa90c2531383431a95dcfb2291f09
Amjad Nashwan <amjad455nash@gmail.com>
2025-09-28 00:16:58 +0300

    🎨 إضافة خلفية متدرجة الألوان لشاشة لوحة التحكم

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -11,3 +11,13 @@ return Scaffold(
 appBar: AppBar(title: const Text("لوحة التحكم")),
 body: Container(), // مؤقت
 );
+body: Container(
+padding: const EdgeInsets.all(20),
+decoration: const BoxDecoration(
+gradient: LinearGradient(
+colors: [Colors.white, Colors.blueGrey],
+begin: Alignment.topCenter,
+end: Alignment.bottomCenter,
+),
+),
+),

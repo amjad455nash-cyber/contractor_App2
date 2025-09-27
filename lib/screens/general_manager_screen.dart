8d537e5f788ced1e2d347f259d023fde5fd7206e
Amjad Nashwan <amjad455nash@gmail.com>
2025-09-28 00:34:48 +0300

    🏗️ إنشاء الكلاس GeneralManagerScreen كوريث من StatelessWidget

--- lib/screens/general_manager_screen.dart
+++ lib/screens/general_manager_screen.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+class GeneralManagerScreen extends StatelessWidget {
+  const GeneralManagerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // مؤقت
+  }
+}

73ff271863a411c0e67f3fe24e501e8407cc145e
Amjad Nashwan <amjad455nash@gmail.com>
2025-09-27 23:40:55 +0300

    📂 استدعاء جميع الشاشات (Login, Dashboard, Manager, Accountants, Developers)

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+import 'screens/dashboard_screen.dart';
+import 'screens/general_manager_screen.dart';
+import 'screens/accountants_screen.dart';
+import 'screens/developers_screen.dart';

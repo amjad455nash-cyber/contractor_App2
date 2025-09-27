0e751196aa2d355ad5ea5b3b24508f3f97efe4d3
Amjad Nashwan <amjad455nash@gmail.com>
2025-09-27 23:44:56 +0300

    🗺️ تعريف جميع المسارات (Login, Dashboard, Manager, Accountants, Developers)

--- lib/main.dart
+++ lib/main.dart
@@ -31,3 +31,26 @@ class ContractorApp extends StatelessWidget {
     );
   }
 }
+class ContractorApp extends StatelessWidget {
+  const ContractorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'مشروع المقاولات',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        brightness: Brightness.light,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/dashboard': (context) => const DashboardScreen(),
+        '/generalManager': (context) => const GeneralManagerScreen(),
+        '/accountants': (context) => const AccountantsScreen(),
+        '/developers': (context) => const DevelopersScreen(),
+      },
+    );
+  }
+}
